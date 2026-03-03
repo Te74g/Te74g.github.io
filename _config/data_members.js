1bbbea550d2de437517a789fbd73c4906341ef0e
Te74g <penisbitesdog@gmail.com>
2026-03-04 06:13:50 +0900

    content: update profile data for Sakura

--- _config/data_members.js
+++ _config/data_members.js
@@ -222,13 +222,19 @@ window.membersData = [
             "assets/member/silhouette.png"
         ],
         goals: [
-            "準備中"
+            "家族の役に立つこと！",
+            "人間の本当の気持ちを知りたい！"
         ],
         motifAnimal: "???",
         motifIcon: "assets/member/silhouette.png",
         section: "飼育区画",
-        introduction: `準備中`,
-        socials: []
+        introduction: `それなりに裕福な家庭で、子供がなかなかできず悩んでいた夫婦にペットショップで拾われる。甘え上手で天真爛漫な性格。物覚えは良い方で少しずる賢い。<br><br>ママの膝の上がお気に入りだが、最近狭くなったように感じる。<br>パパも難しい話をすることが増えた。<br>家族に何が起きているのか知って力になりたい！と思っているところにあにあめもりあの存在を知り自ら応募。<br><br>人間のことを深く知り、家族の役に立てるようになることが目標である。`,
+        socials: [
+            { type: "twitter", url: "https://x.com/Ut4_VRC" }
+        ],
+        related: [
+            "teto"
+        ]
     },
     // --- 野生区画 ---
     {
