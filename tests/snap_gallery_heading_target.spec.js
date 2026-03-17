a8db06f3779ef9788db736483ba584d6554f5dec
Te74g <penisbitesdog@gmail.com>
2026-03-18 06:01:21 +0900

    chore: remove accidental files and ignore local tool dirs

--- tests/snap_gallery_heading_target.spec.js
+++
@@ -1,9 +0,0 @@
-﻿const { test } = require('@playwright/test');
-
-test('snap gallery heading target', async ({ page }) => {
-  await page.addInitScript(() => window.sessionStorage.setItem('maintenanceBypass', 'true'));
-  await page.setViewportSize({ width: 1440, height: 949 });
-  await page.goto('/gallery/', { waitUntil: 'networkidle' });
-  await page.waitForTimeout(250);
-  await page.screenshot({ path: 'tmp_gallery_heading_target.png', fullPage: false });
-});
